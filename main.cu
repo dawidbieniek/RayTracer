bc768db2142fcb6757ed6950266898df4b2e646c
dawid bieniek <01159194@pw.edu.pl>
2023-06-03 17:40:22 +0200

    Simple circle

--- main.cu
+++ main.cu
@@ -23,32 +23,45 @@ void check_cuda(cudaError_t result, char const* const func, const char* const fi
 
 #define checkCudaErrors(val) check_cuda( (val), #val, __FILE__, __LINE__ )
 // !TMP
+#include "vec3.h"
+#include "ray.h"
 
-int screenWidth = 640;
+int screenWidth = 960;
 int screenHeight = 480;
 
-cudaError_t addWithCuda(int* c, const int* a, const int* b, unsigned int size);
-
-__global__ void addKernel(int* c, const int* a, const int* b)
+__device__ bool hit_sphere(const vec3& center, float radius, const ray& r) 
 {
-	int i = threadIdx.x;
-	c[i] = a[i] + b[i];
+	vec3 oc = r.origin() - center;
+	float a = dot(r.direction(), r.direction());
+	float b = 2.0f * dot(oc, r.direction());
+	float c = dot(oc, oc) - radius * radius;
+	float discriminant = b * b - 4.0f * a * c;
+	return (discriminant > 0.0f);
 }
 
-__global__ void render(float* fb, int maxX, int maxY)
+__device__ vec3 color(const ray& r) 
+{
+	if (hit_sphere(vec3(0, 0, -1), 0.5, r))
+		return vec3(1, 0, 0);
+	// Tlo
+	vec3 unitDirection = unit_vector(r.direction());
+	float t = 0.5f * (unitDirection.y() + 1.0f);
+	return (1.0f - t) * vec3(1.0, 1.0, 1.0) + t * vec3(0.5, 0.7, 1.0);
+}
+
+__global__ void render(vec3* fb, int maxX, int maxY, vec3 lowerLeftCorner, vec3 horizontal, vec3 vertical, vec3 origin)
 {
 	int i = threadIdx.x + blockIdx.x * blockDim.x;
 	int j = threadIdx.y + blockIdx.y * blockDim.y;
-
 	if ((i >= maxX) || (j >= maxY)) return;
-
-	int pixelIndex = j * maxX * 3 + i * 3;
-	fb[pixelIndex + 0] = float(i) / maxX;	// R
-	fb[pixelIndex + 1] = float(j) / maxY;	// G
-	fb[pixelIndex + 2] = 0.2;				// B
+	int pixel_index = j * maxX + i;
+	float u = float(i) / float(maxX);
+	float v = float(j) / float(maxY);
+	ray r(origin, lowerLeftCorner + u * horizontal + v * vertical);
+	fb[pixel_index] = color(r);
 }
 
-float* fb;
+vec3* fb;
 
 void draw()
 {
@@ -59,10 +72,9 @@ void draw()
 	{
 		for (int x = 0; x < screenWidth; x++)
 		{
-			int ind = 3*(y * screenWidth + x);
+			int ind = y * screenWidth + x;
 
-			glColor3f(fb[ind], fb[ind + 1], fb[ind + 2]);
-			//std::cout << fb[ind] << "  " << fb[ind + 1] << "  " << fb[ind + 2] << std::endl;
+			glColor3f(fb[ind].r(), fb[ind].g(), fb[ind].b());
 			glVertex2f(x, y);
 		}
 	}
@@ -93,17 +105,7 @@ int main(int argc, char** argv)
 
 	// Good
 	int numPixels = screenWidth * screenHeight;
-	size_t fbSize = 3 * static_cast<size_t>(numPixels) * sizeof(float);
-	//fb = (float*)malloc(fbSize);
-	//for (int y = 50; y < 100; y++)
-	//{
-	//	for (int x = 0; x < screenWidth; x++)
-	//	{
-	//		fb[3*(y * screenWidth + x)] = 1.0;
-	//		fb[3 * (y * screenWidth + x) + 1] = 0.5;
-	//		fb[3 * (y * screenWidth + x) + 2] = 1.0;
-	//	}
-	//}
+	size_t fbSize = numPixels * sizeof(vec3);
 	checkCudaErrors(cudaMallocManaged(&fb, fbSize));
 
 	int tx = 8;
@@ -111,7 +113,7 @@ int main(int argc, char** argv)
 	dim3 blocks(screenWidth / tx + 1, screenHeight / ty + 1);
 	dim3 threads(tx, ty);
 
-	render <<<blocks, threads >>> (fb, screenWidth, screenHeight);
+	render <<<blocks, threads >>> (fb, screenWidth, screenHeight, vec3(-2.0, -1.0, -1.0), vec3(4.0, 0.0, 0.0), vec3(0.0, 2.0, 0.0), vec3(0.0, 0.0, 0.0));
 	checkCudaErrors(cudaGetLastError());
 	checkCudaErrors(cudaDeviceSynchronize());
 
@@ -153,84 +155,4 @@ int main(int argc, char** argv)
 	//}
 
 	return 0;
-}
-
-// Helper function for using CUDA to add vectors in parallel.
-cudaError_t addWithCuda(int* c, const int* a, const int* b, unsigned int size)
-{
-	int* dev_a = 0;
-	int* dev_b = 0;
-	int* dev_c = 0;
-	cudaError_t cudaStatus;
-
-	// Choose which GPU to run on, change this on a multi-GPU system.
-	cudaStatus = cudaSetDevice(0);
-	if (cudaStatus != cudaSuccess) {
-		fprintf(stderr, "cudaSetDevice failed!  Do you have a CUDA-capable GPU installed?");
-		goto Error;
-	}
-
-	// Allocate GPU buffers for three vectors (two input, one output)    .
-	cudaStatus = cudaMalloc((void**)&dev_c, size * sizeof(int));
-	if (cudaStatus != cudaSuccess) {
-		fprintf(stderr, "cudaMalloc failed!");
-		goto Error;
-	}
-
-	cudaStatus = cudaMalloc((void**)&dev_a, size * sizeof(int));
-	if (cudaStatus != cudaSuccess) {
-		fprintf(stderr, "cudaMalloc failed!");
-		goto Error;
-	}
-
-	cudaStatus = cudaMalloc((void**)&dev_b, size * sizeof(int));
-	if (cudaStatus != cudaSuccess) {
-		fprintf(stderr, "cudaMalloc failed!");
-		goto Error;
-	}
-
-	// Copy input vectors from host memory to GPU buffers.
-	cudaStatus = cudaMemcpy(dev_a, a, size * sizeof(int), cudaMemcpyHostToDevice);
-	if (cudaStatus != cudaSuccess) {
-		fprintf(stderr, "cudaMemcpy failed!");
-		goto Error;
-	}
-
-	cudaStatus = cudaMemcpy(dev_b, b, size * sizeof(int), cudaMemcpyHostToDevice);
-	if (cudaStatus != cudaSuccess) {
-		fprintf(stderr, "cudaMemcpy failed!");
-		goto Error;
-	}
-
-	// Launch a kernel on the GPU with one thread for each element.
-	addKernel <<<1, size >>> (dev_c, dev_a, dev_b);
-
-	// Check for any errors launching the kernel
-	cudaStatus = cudaGetLastError();
-	if (cudaStatus != cudaSuccess) {
-		fprintf(stderr, "addKernel launch failed: %s\n", cudaGetErrorString(cudaStatus));
-		goto Error;
-	}
-
-	// cudaDeviceSynchronize waits for the kernel to finish, and returns
-	// any errors encountered during the launch.
-	cudaStatus = cudaDeviceSynchronize();
-	if (cudaStatus != cudaSuccess) {
-		fprintf(stderr, "cudaDeviceSynchronize returned error code %d after launching addKernel!\n", cudaStatus);
-		goto Error;
-	}
-
-	// Copy output vector from GPU buffer to host memory.
-	cudaStatus = cudaMemcpy(c, dev_c, size * sizeof(int), cudaMemcpyDeviceToHost);
-	if (cudaStatus != cudaSuccess) {
-		fprintf(stderr, "cudaMemcpy failed!");
-		goto Error;
-	}
-
-Error:
-	cudaFree(dev_c);
-	cudaFree(dev_a);
-	cudaFree(dev_b);
-
-	return cudaStatus;
 }
